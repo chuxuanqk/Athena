777571b98d6b40942b34976ed3df1d112a5e69ac
Saber <642964056@qq.com>
2019-01-17 11:37:59 +0800

    建立项目结构

---
+++ App/Apollo/Painter/Painter_Ui.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+__author__ = 'Saber'
+__date__ = '17/1/19 上午10:39'
+
+
+from PyQt5.QtWidgets import QWidget
---
+++ core/main.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+__author__ = 'Saber'
+__date__ = '17/1/19 上午10:31'
